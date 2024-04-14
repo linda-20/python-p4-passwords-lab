8d52c3a77b44c6dffdcdc9d1186bfb98459655ab
linda-20 <lindaaron40@gmail.com>
2024-04-14 11:56:00 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -16,7 +16,6 @@ class ClearSession(Resource):
         return {}, 204
 
 class Signup(Resource):
-    
     def post(self):
         json = request.get_json()
         user = User(
@@ -28,16 +27,47 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
+    def get(self):
+        
+        user_id = session.get('user_id')
+        if user_id is not None:
+            user = User.query.get(user_id)
+            if user is not None:
+                return user.to_dict(), 200
+        return {}, 204
+
     pass
 
 class Login(Resource):
+    def post(self):
+
+        json = request.get_json()
+        username = json.get('username')
+        password = json.get('password')
+        if not username or not password:
+            return {}, 400
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {}, 401
+
     pass
 
 class Logout(Resource):
+    def delete(self):
+
+        session['user_id'] = None
+
+        return {}, 204
     pass
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
